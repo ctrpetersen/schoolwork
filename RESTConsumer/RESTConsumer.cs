3e1420fefc36b331492acb1bc0a50d894de0581f
Christoffer Petersen <ctrpetersen@outlook.com>
2018-09-20 09:59:15 +0200

    Time elapsed + fixed put

--- RESTConsumer/RESTConsumer.cs
+++ RESTConsumer/RESTConsumer.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Timers;
 using Newtonsoft.Json;
 
 namespace RESTConsumer
@@ -17,21 +19,40 @@ namespace RESTConsumer
 
         public void Start()
         {
+            Stopwatch t = new Stopwatch();
             Console.WriteLine("GETALL https://jsonplaceholder.typicode.com/todos");
+            t.Start();
             IList<RESTData> dataL = GetAll("https://jsonplaceholder.typicode.com/todos");
             Console.WriteLine(dataL[0].id + dataL[0].userId + dataL[0].title);
+            t.Stop();
+            Console.WriteLine($"Time elapsed {t.Elapsed}\n");
 
             Console.WriteLine("GETONE https://jsonplaceholder.typicode.com/todos/3");
+            t.Reset();
+            t.Start();
             Console.WriteLine(GetOne("https://jsonplaceholder.typicode.com/todos/3").userId);
+            Console.WriteLine($"Time elapsed {t.Elapsed}\n");
 
             Console.WriteLine("DELETE https://jsonplaceholder.typicode.com/todos/3");
+            t.Reset();
+            t.Start();
             Console.WriteLine(Delete("https://jsonplaceholder.typicode.com/todos/3"));
+            Console.WriteLine($"Time elapsed {t.Elapsed}\n");
 
             Console.WriteLine("POST https://jsonplaceholder.typicode.com/todos");
+            t.Reset();
+            t.Start();
             Console.WriteLine(Post(new RESTData(10, 20, "1020"), "https://jsonplaceholder.typicode.com/todos"));
+            Console.WriteLine($"Time elapsed {t.Elapsed}\n");
 
             Console.WriteLine("PUT https://jsonplaceholder.typicode.com/todos/3");
-            Console.WriteLine(Put(new RESTData(1, 1, "1020"), "https://jsonplaceholder.typicode.com/todos"));
+            t.Reset();
+            t.Start();
+            Console.WriteLine(Put(new RESTData(1, 1, "1020"), "https://jsonplaceholder.typicode.com/todos/3"));
+            Console.WriteLine($"Time elapsed {t.Elapsed}\n");
+            t.Reset();
+
+            Console.WriteLine("All done!");
         }
 
         private IList<RESTData> GetAll(string url)
