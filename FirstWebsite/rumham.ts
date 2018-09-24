52d51b1ddffbbda1cb929267ef808024c7fb2e9a
Christoffer Petersen <ctrpetersen@outlook.com>
2018-09-24 10:42:01 +0200

    rumham.ts

---
+++ FirstWebsite/rumham.ts
@@ -0,0 +1,14 @@
+class rumham
+{
+    public Mantis(heck: string){
+        return "Toboggan, MD."
+    }
+
+    public ManSpider(){
+        this.Mantis("WHATUP!!!")
+        let n = "40"
+        let y = 40
+        return n+y
+
+    }
+}
