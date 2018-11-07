c247c5ee2df386d52e500f29523fa638a550452c
Christoffer Petersen <ctrpetersen@outlook.com>
2018-11-07 09:36:51 +0100

    Fikset promise<any>

--- Hangman/src/js/secretword.ts
+++ Hangman/src/js/secretword.ts
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 export class SecretWord {
-    static async getWord(maxWordLength: number, minWordLength: number): Promise<object> {
+    static async getWord(maxWordLength: number, minWordLength: number): Promise<any> {
         var response = await axios.get('http://api.evang.dk/randomwords/', { 'headers': { 'numberOfWords': 1, 'maxWordLength': maxWordLength, 'minWordLength': minWordLength } });
         return response;
     }
